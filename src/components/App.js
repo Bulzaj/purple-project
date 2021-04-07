628934fb9bbe0971f3dc1685b6afb2c9a41d8a18
bulzaj <ernestsado94@gmail.com>
2021-04-07 11:17:39 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Workspace from "./workspace/Workspace";
+
+function App() {
+  return (
+    <div className="app-container">
+      <main>
+        <Workspace />
+      </main>
+    </div>
+  );
+}
+
+export default App;
