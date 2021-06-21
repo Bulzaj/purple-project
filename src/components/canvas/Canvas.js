2f102179f229ec2ed643c9d5089de742f50c7b3e
bulzaj <ernestsado94@gmail.com>
2021-06-21 19:21:10 +0200

    remove test elements

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 
 import useDrawer from "../../hooks/useDrawer";
 import usePanning from "../../hooks/usePanning";
 import useZooming from "../../hooks/useZooming";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Viewbox from "../viewbox/viewbox";
 
 const Canvas = (props) => {
-  const canvasRef = useRef();
   const items = useDrawer(props.pipeNetwork);
   const { handlers: panHandlers } = usePanning();
   const { handlers: zoomHandlers } = useZooming();
@@ -18,7 +17,6 @@ const Canvas = (props) => {
 
   return (
     <svg
-      ref={canvasRef}
       className="canvas"
       overflow="hidden"
       onMouseDown={panHandlers.handleMouseDown}
@@ -27,8 +25,6 @@ const Canvas = (props) => {
       onWheel={zoomHandlers.handleWheel}
     >
       <Viewbox translate={position} scale={scale}>
-        <circle cx={0} cy={0} r={100} />
-        <rect x={550} y={320} width={100} height={100} />
         {items}
       </Viewbox>
     </svg>
